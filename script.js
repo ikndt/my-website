446fbb2831540fd5b39dceef74dfed7dda72e461
ikndt <asharongisselle@gmail.com>
2026-08-29 06:05:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function sayhello() {
-    alert("Hello! "); 
+function goToThoughts() {
+    document.getElementById("thoughts").scrollIntoView({
+        behavior: "smooth"
+    });
 }
+const thoughts = document.querySelectorAll(".thought");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+thoughts.forEach((thought) => {
+    observer.observe(thought);
+})
